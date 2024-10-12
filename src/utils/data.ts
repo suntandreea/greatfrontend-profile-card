3f6b412aa30bf39b7e9a24975c8459723ed5a129
Andreea <suntandreea@protonmail.com>
2024-10-12 20:43:17 +0300

    responsive

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -14,17 +14,17 @@ export const users: UserProfile[] = [{
     twitter: "#",
   }
 },
-  // {
-  //   id: 2,
-  //   image: "https://picsum.photos/80",
-  //   name: "Andreea Roxana Cozariuc Munteanu",
-  //   role: "Certified Bacon Engineer, Web Developer and Tester for Microsoft @ Allyis Tech Mahindra",
-  //   bio: "Bacon ipsum dolor amet jerky nulla pork belly tri-tip deserunt chicken laborum ut ut commodo sint est incididunt exercitation. Esse jerky andouille irure, elit biltong short ribs dolore mollit labore do porchetta bacon. Flank corned beef velit anim nisi ground round drumstick in. Tenderloin ipsum dolor frankfurter voluptate bacon nostrud kielbasa flank pork chop meatloaf. Minim pork belly id prosciutto dolor filet mignon, ad turducken. T-bone officia pork chop id, ex kevin laborum tempor.",
-  //   contact: {
-  //     github: "#",
-  //     linkedin: "#",
-  //     instagram: "#",
-  //     twitter: "#",
-  //   }
-  // }
+  {
+    id: 2,
+    image: "https://picsum.photos/80",
+    name: "Andreea Roxana Cozariuc Munteanu",
+    role: "Certified Bacon Engineer, Web Developer and Tester for Microsoft @ Allyis Tech Mahindra",
+    bio: "Bacon ipsum dolor amet jerky nulla pork belly tri-tip deserunt chicken laborum ut ut commodo sint est incididunt exercitation. Esse jerky andouille irure, elit biltong short ribs dolore mollit labore do porchetta bacon. Flank corned beef velit anim nisi ground round drumstick in. Tenderloin ipsum dolor frankfurter voluptate bacon nostrud kielbasa flank pork chop meatloaf. Minim pork belly id prosciutto dolor filet mignon, ad turducken. T-bone officia pork chop id, ex kevin laborum tempor.",
+    contact: {
+      github: "#",
+      linkedin: "#",
+      instagram: "#",
+      twitter: "#",
+    }
+  }
 ]
